915d1697dada9920b74aedf618f5def7051d2192
Paulo Rogerio <paulo@intranet>
2025-01-28 10:42:39 -0300

    fix: Ajustes

--- build/middleware/routeNotFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routeNotFound = routeNotFound;
-function routeNotFound(req, res, next) {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    logging.error(error);
-    res.status(404).json({ error: error.message });
-    return;
-}
